288fdbaff2c95fb5178989ad690c278339db2a02
Rochelle Kris <rhk79@cornell.edu>
2020-11-09 19:33:53 -0500

    display pacman

--- main.ml
+++ main.ml
@@ -1,28 +1,38 @@
 open Graphics
 open Map
-open Player
+(**open Player*)
 
 let window_width = 1500
 let window_height = 750
 let map_width = 1300
 let map_height = 550
 
+
 let set_window (title: string) (color: Graphics.color) : unit = 
   set_window_title title;
   set_color black;
   fill_rect 0 0 (size_x ()) (size_y ())
 
-let parse_dir (dir: string) =
-  match dir with 
-  |"\033[A" -> "UP"
-  |"\033[B" -> "DOWN"
-  |"\033[D" -> "LEFT"
-  |"\033[C" -> "RIGHT"
-  |_ -> ""
 
-let rec loop () = 
+
+
+let parse_dir (dir: string) (plyr: Player.t)=
+  let dir_move = 
+    match dir with 
+    |"\033[A" -> (0,50)
+    |"\033[B" -> (0,-50)
+    |"\033[D" -> (-50,0)
+    |"\033[C" -> (50,0)
+    |_ -> (0,0)
+  in 
+  dir_move
+(**move plyr dir_move*)
+
+
+let rec loop () = (**plyr = *)
   Unix.sleep(1);
-  ignore (parse_dir (read_line ()));
+  (**ignore (parse_dir (read_line ()) plyr);
+     loop () plyr*)
   loop ()
 
 let main (settings: string) : unit = 
@@ -34,7 +44,11 @@ let main (settings: string) : unit =
   draw_rect 100 100 map_width map_height;
   let map = make_map map_width map_height (100,100) in 
   draw_map map;
-  draw_player new_player; 
+  let create_sprite = 
+    Graphics.set_color (rgb 255 255 0);
+    Graphics.draw_circle 175 175 25;
+    Graphics.display_mode true in
+  create_sprite;
   ignore (loop ());
   ()
 
--- player.ml
+++ player.ml
@@ -3,12 +3,15 @@ open Images
 open Sprite
 
 type t = {
-  image : Sprite.t;
+  (*image : Graphics.image;*)
   mutable x : int;
   mutable y : int;
 }
 
-let new_player = {image = Sprite.make_sprite "pacman.png"; x = 0; y = 0}
+
+
+(**let new_player = {image = Sprite.make_sprite "pacman.png"; x = 0; y = 0}*)
+let new_player = {x = 0; y = 0}
 
 let get_position player = 
   (player.x, player.y)
@@ -17,7 +20,13 @@ let move (player : t) (dir : int * int) =
   player.x <- fst (get_position player) + fst dir; 
   player.y <- snd (get_position player) + snd dir 
 
+
 let draw_player user = 
-  display_mode true;
-  ignore (user.image);
-  ()
+  failwith "Unimplemented"
+(**  display_mode true;
+     ignore (user.image);
+     ()
+*)
+
+
+
