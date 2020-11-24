712bed875a9870c809e5cdd23ed4c2d62e1ef1cb
Rochelle Kris <rhk79@cornell.edu>
2020-11-24 18:19:00 -0500

    adding in make bisect commands

--- test.ml
+++ test.ml
@@ -1,31 +1,34 @@
 open OUnit2
 open Player
-(*open Ghost
-  open Main
-  open Map*)
+open Ghost
+open Main
+open Map
 
 
 let pp_tuple coordinates = 
   "( " ^ string_of_int (fst coordinates) ^ " , " ^ 
   string_of_int (snd coordinates) ^ " )"
 
-let player_move_test  
+let player_move_pos_test  
     (name : string) 
     (player : Player.t)
     (input_dir : int * int)
     (expected_output : int * int) : test = 
   name >:: (fun _ -> 
-      assert_equal expected_output (move player input_dir; get_position player)
-        ~printer:pp_tuple)
+      assert_equal expected_output (Player.move player input_dir; 
+                                    get_position player) ~printer:pp_tuple)
 
 let player_1 = new_player
 
 let player_tests =
   [
-    player_move_test "Initial position (0,0)" player_1 (0,0) (0,0);
-    player_move_test "Initial player moves up 1" player_1 (0,1) (0,1);
-    player_move_test "After moving up 1, move right 1" player_1 (1,0) (1,1);
+    player_move_pos_test "Initial position at (175,175)" player_1 (0,0) (175,175);
+    player_move_pos_test "Initial player moves up 50" player_1 (175,175) (175,225);
+    player_move_pos_test "After moving up 50, move right 50" player_1 (175,225) 
+      (225,225);
   ]
+
+
 
 let suite =
   "test suite"  >::: List.flatten [
